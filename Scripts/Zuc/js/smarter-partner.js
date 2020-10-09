46ed815104a473d239bf6de3ad2dc09f863324d2
Hima <hvyas738@gmail.com>
2020-10-09 12:34:16 -0400

    first commit

--- Scripts/Zuc/js/smarter-partner.js
+++ Scripts/Zuc/js/smarter-partner.js
@@ -5,26 +5,41 @@ if(currentPath.indexOf('smarterpartner')>0){
   playVideo();
 }
 
+
 function playVideo(){
-  $('.video-box').click(function(){
     $('.video-modal-container').removeClass('non-visible');
     let mainVideoClass = $('.video-modal');
     let videoTag = $('.modal-video');
     videoTag.empty();
-    let title = $(this).find('p').text();
-    mainVideoClass.find('h2').text(title);
+    // let title = $(this).find('p').text();
+    // mainVideoClass.find('h2').text(title);
     let videoLink = $(this).find('.video-link').text();
     $('.modal-video').append('<source src="'+videoLink+'" type="video/mp4">');
-  });
+}
+$('.video-box').click(playVideo);
+
+let videoBoxes = document.querySelectorAll('.video-box');
+let currentURL = window.location.href;
+let getNumber = 0;
+let indexOf = currentURL.indexOf('#');
+getNumber = currentURL.slice(indexOf + 1);
+getNumber -= 1;
+console.log(getNumber);
+if(indexOf > 0 && getNumber < 5){
+  videoBoxes[getNumber].click(playVideo);
+}else{
+  $('.video-modal-container').addClass('non-visible');
 }
 
+
 $('.video-modal .close-modal').click(function(){
-  // console.log('should close it');
+    // console.log('should close it');
     $('.video-modal-container').addClass('non-visible');
     let vid = document.getElementById('clicked-video');
   	vid.load();
   	$('.modal-video').empty();
   });
+
 $('.becomeSP-formOpen').click(function(){
   $('.becomePartner-form-container').removeClass('non-visible');
 
@@ -32,4 +47,8 @@ $('.becomeSP-formOpen').click(function(){
 $('.becomePartner-form').find('.close-modal').click(function(){
    $('.becomePartner-form-container').addClass('non-visible');
   });
+
+//
+
+
 }); //main document ends
